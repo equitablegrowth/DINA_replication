5ebdde4a5e6cb908fdac239c90fc952d8de4a34d
Austin Clemens <austinc@mac.com>
2017-02-03 14:08:26 -0500

    first commit

---
+++ DINA_Stata_replication.do
@@ -0,0 +1,40 @@
+* put the variables you want to analyze here, after ds
+ds fainc
+local varl r(varlist)
+
+* loop through variables
+foreach var in `varl'{
+	display "---------",`var',"---------"
+
+	* split income equally between spouses and weight
+	egen equal=mean(``var''),by(year id)
+	gen equal_weighted=equal*dweght
+
+	* loop through all years in the dataset
+	levelsof year, local(years)
+	foreach y of local years{
+		* get percentiles of equal weighted by dweght
+		cap: summarize equal [aweight=dweght] if year==`y',detail
+		local p50 r(p50)
+		local p90 r(p90)
+
+		* generate a new variable used for storing the group each row is in (low/middle/high income)
+		gen equal_rank=0
+		cap: replace equal_rank=1 if equal>`p50'
+		cap: replace equal_rank=2 if equal>`p90'
+
+		* get sum of equal_weighted for each group
+		cap: sum equal_weighted if year==`y' & equal_rank==0
+		local bottom_total `r(sum)'
+		cap: sum equal_weighted if year==`y' & equal_rank==1
+		local middle_total `r(sum)'
+		cap: sum equal_weighted if year==`y' & equal_rank==2
+		local top_total `r(sum)'
+
+		* display
+		display `y',(100*`bottom_total'/(`bottom_total'+`middle_total'+`top_total')),(100*`middle_total'/(`bottom_total'+`middle_total'+`top_total')),(100*`top_total'/(`bottom_total'+`middle_total'+`top_total'))
+		drop equal_rank
+	}
+	drop equal_weighted
+	drop equal
+}
---
+++ DINA_r_replication.r
@@ -0,0 +1,40 @@
+library(foreign)
+install.packages("devtools")
+devtools::install_github("hadley/bigvis")
+library(bigvis)
+
+df<-read.csv(file.choose())
+
+# list of vars we want to summarize
+vars=c('fiinc','fninc','fainc','flinc','fkinc','ptinc','plinc','pkinc','diinc','princ','peinc','poinc','hweal')
+
+# split income equally between spouses
+for(var in vars){
+	df[[paste(var,'_split',sep='')]] <- ave(df[[var]], df$year, df$id, FUN=mean)
+}
+
+# create new variables to group observations by category: bottom 50, middle 40, and top 10
+for(var in vars){
+	q_vector=c()
+	for(y in list(set(df['year']))){
+		temp=subset(df,year==y)
+		q_vector=c(q_vector,cut(temp[[paste(var,'_split',sep='')]],c(-Inf,weighted.quantile(temp[[paste(var,'_split',sep='')]], temp$dweght, probs=c(.5,.9)),Inf),labels=c(0,1,2)))
+	}
+	df[[paste(var,'_q',sep='')]]<-q_vector
+	df[[paste(var,'_weght',sep='')]]<-df[[paste(var,'_split',sep='')]]*df$dweght
+}
+
+# print results
+for(var in vars){
+	varweght=paste(var,'_weght',sep='')
+	varq=paste(var,'_q',sep='')
+	print(varq)
+	for(y in list(set(df['year']))){
+		temp=df[df[,'year']==y,]
+		q1total=sum(temp[temp[[varq]]==1,][[varweght]])
+		q2total=sum(temp[temp[[varq]]==2,][[varweght]])
+		q3total=sum(temp[temp[[varq]]==3,][[varweght]])
+		total=q1total+q2total+q3total
+		print(paste(y,q1total,q2total,q3total,total,y,q1total/total,q2total/total,q3total/total,sep=','))
+	}
+}
